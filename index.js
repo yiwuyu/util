0fb3418eaca5dadfefc3da4db98a5abb0b5c43af
lichin <yiwuyu.it@gmail.com>
2018-02-05 18:11:13 +0800

    Uniform TAB

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const getFileSize = (value = 0) => {
   }
   let map = recursion(value, 0)
   let unitMap = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-	const index = map.size.toString().indexOf('.')
-	if (index < 0) {
-		// 无小数点
-		return map.size + unitMap[map.index]
-	} else {
-		// 有小数点
-		return parseFloat(map.size.toString().substring(0, index + 3)) + unitMap[map.index]
-	}
+  const index = map.size.toString().indexOf('.')
+  if (index < 0) {
+    // 无小数点
+    return map.size + unitMap[map.index]
+  } else {
+    // 有小数点
+    return parseFloat(map.size.toString().substring(0, index + 3)) + unitMap[map.index]
+  }
 }
